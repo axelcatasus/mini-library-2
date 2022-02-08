551ea388c62e2342696bbd27b931d149c79ef103
axelcatasus <84475869+axelcatasus@users.noreply.github.com>
2022-02-08 11:08:24 +0100

    Add components
    Change default components to new components
    Initialize routing and store
    Create JSON-database with books in assets
    Basic styling

---
+++ src/components/Book.vue
@@ -0,0 +1,33 @@
+<template>
+  <article>
+    <h2>{{book.title}}</h2>
+    <span>{{book.author}}</span>
+  </article>
+</template>
+
+<script>
+export default {
+  props:['book'],
+  
+
+}
+</script>
+
+<style lang="sass">
+
+article
+  background: tomato
+  height: 18rem
+  width: 12rem
+  padding: 1rem
+  display: flex
+  flex-direction: column
+  justify-content: flex-end
+
+.beeg
+  height: 27rem
+  width: 18rem
+  background: purple
+
+
+</style>
---
+++ src/views/SingleBookView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="single-book-view">
+    <Book :book="book" :class='{beeg: beeg}'/>
+  </div>
+</template>
+
+<script>
+import Book from '../components/Book.vue'
+
+export default {
+  components: {Book},
+  computed: {
+    book(){
+      return this.$store.state.books.find(book => book.id == this.$route.params.id)
+    },
+  },
+  data(){return{
+    beeg: true
+  }}
+}
+</script>
+
+<style scoped lang="sass">
+  .single-book-view
+    display: grid
+    place-items: center
+    min-height: 100vh
+</style>
